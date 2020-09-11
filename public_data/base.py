f0e54b6643842972ac665cd6f8f13d87b8b3e6f9
ukamoy <rinzia@msn.com>
2020-09-11 17:46:27 +0800

    add open market operation

--- public_data/base.py
+++ public_data/base.py
@@ -1,6 +1,8 @@
 import os
+import re
 import logging
 import requests
+import execjs
 from lxml import etree
 import json
 import hmac
@@ -17,9 +19,9 @@ headers = {
 class BaseControl:
     
     @classmethod
-    def send_requests(cls, url, method="GET", headers=headers):
+    def send_requests(cls, url, method="GET", headers=headers, params=None, json=None):
         try:
-            resp = requests.request(method, url, headers=headers, timeout=8)
+            resp = requests.request(method, url, headers=headers, params=params, json=json, timeout=8)
         except Exception as e:
             resp = requests.Response()
             resp.status_code = 400
@@ -27,15 +29,34 @@ class BaseControl:
         return resp
 
     @classmethod
+    def getJSPage(cls, URL):
+        session = requests.session()
+        jsPage = session.get(URL).text
+        js = re.findall(r'<script type="text/javascript">([\w\W]*)</script>', jsPage)[0]
+        js = re.sub(r'atob\(', 'window["atob"](', js)
+        js2 = f'function getURL(){{ var window = {{}};{js} return window["location"];}}'
+        ctx = execjs.compile(js2)
+        tail = ctx.call('getURL')
+        URL2 = cls.parse_new_url(URL, tail)
+        page = session.get(URL2)
+        page.encoding = 'UTF-8'
+        return page
+    
+    @classmethod
+    def parse_new_url(cls, url, new_route):
+        return urllib.parse.urljoin(url, new_route)
+
+    @classmethod
     def parse_html(cls, html, reg_exp):
         text = etree.HTML(html)
         return text.xpath(reg_exp)
 
     @classmethod
-    def unpack_row(cls, row, sp_treat={}):
-        elts = row.xpath(f'.//td')
+    def unpack_row(cls, row, tag=".//td", sp_treat={}):
+        elts = row.xpath(tag)
         resp = []
         for idx, val in enumerate(elts):
+            print(val.text)
             treat = sp_treat.get(idx, "")
             if treat:
                 val = ''.join(val.xpath(treat))
@@ -51,3 +72,7 @@ class BaseControl:
     @classmethod
     def DataFrame(cls, matrix, columns=None):
         return pd.DataFrame(matrix, columns=columns)
+
+    @classmethod
+    def current(cls):
+        return datetime.now()
--- public_data/shibor.py
+++ public_data/shibor.py
@@ -1,18 +1,45 @@
 from base import BaseControl
 shibor_url = "http://www.shibor.org/shibor/web/html/shibor.html"
+omo_url = "http://www.pbc.gov.cn/zhengcehuobisi/125207/125213/125431/index.html"
 
-class Shibor(BaseControl):
+class CenterBank(BaseControl):
+
     @classmethod
-    def read_data(cls):
+    def Shibor(cls):
         resp = cls.send_requests(shibor_url)
-        rows = cls.parse_html(resp.content.decode("gb2312"), f'//table[@class="shiborquxian"]/tr')
+        rows = cls.parse_html(resp.content.decode("gb2312"), '//table[@class="shiborquxian"]/tr')
         sp_treat = {1:".//a//font//text()", 3:".//img/@src"}
         table = [cls.unpack_row(td, sp_treat=sp_treat) for td in rows]
         df = cls.DataFrame(table[:-1]).drop(0, axis=1)
-        df.columns = ["Period","Shibor","Side","Trend(BP)"]
-        df["Trend(BP)"] = df.apply(lambda x: float(x["Trend(BP)"]) if "upicon.gif" in x["Side"] else -float(x["Trend(BP)"]), axis=1)
-        df = df.drop("Side", axis=1)
-        return df
+        df[4] = df.apply(lambda x: f"+{x[4].strip()}" if "upicon.gif" in x[3] else f"-{x[4].strip()}", axis=1)
+        df = df.drop(3, axis=1)
+        df.columns = ["Period","Shibor","Trend(BP)"]
+        update_dt = cls.parse_html(resp.content.decode("gb2312"), '//td[@class="infoTitleW"]//text()')
+        return "".join(update_dt).strip(), df
 
-df = Shibor.read_data()
-print(df)
+    @classmethod
+    def OpenMarketOperations(cls):
+        resp = cls.getJSPage(omo_url)
+        links = cls.parse_html(resp.content.decode("utf-8"), '//td[@class="unline"]//a/@href')
+        titles = cls.parse_html(resp.content.decode("utf-8"), '//td[@class="unline"]//a/@title')
+        dates = cls.parse_html(resp.content.decode("utf-8"), '//td[@class="unline"]//span/text()')
+        today_date = cls.current().strftime("%Y-%m-%d")
+        to_link = ""
+        for link, title, date in zip(links, titles, dates):
+            if "公开市场业务交易公告" in title and date == today_date:
+                to_link = link
+
+        operation = ""
+        if to_link:
+            new_url = cls.parse_new_url(omo_url, to_link)
+            info_page = cls.getJSPage(new_url)
+            content = cls.parse_html(info_page.content.decode("utf-8"), '//div[@id="zoom"]/p/text()')
+            table = cls.parse_html(info_page.content.decode("utf-8"), '//div[@id="zoom"]/table/tbody/tr')
+            detail = [cls.unpack_row(row, tag=".//td/p/span/span/span") for row in table]
+            if detail:
+                operation = f"{content[0]}\n\n{detail[0][0]}:{detail[1][0]}天\n{detail[0][1]}:{detail[1][1]}亿元\n{detail[0][2]}:{detail[1][2]}"
+        return operation
+
+
+
+print(CenterBank.OpenMarketOperations())
